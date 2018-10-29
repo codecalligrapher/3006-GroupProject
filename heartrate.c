da463876159f5b62aba990e65535d2cc26296ff5
aadi350 <a.aadi380@gmail.com>
2018-10-29 19:34:15 -0400

    Building Counter
    Not tested as yet

--- heartrate.c
+++ heartrate.c
@@ -1,21 +1,30 @@
-#include <p18f4550.h>
+#include <p18f452.h>
 #include <delays.h>
 #include <timers.h>
-#include <definition.h>
-#include <heartrate.h>
 #include <stdio.h>
+#include "def_f842.h"
+#include "kpad.h"
 
-#define _XTAL_FREQ      4000000
+#define TMR0_RST    WriteTimer0(0)
+#define TMR1_RST    WriteTimer1(0)
+
 #define measure_ready   PORTBbits   .RB0    
-#define heart_in        PORTBbits   .RB2
+#define heart_in        PORTAbits   .T0CKI
+
+#define test_LED_0      LATDbits    .LATD0
+
+#define TEST            0
+#define ALARM_SEQUENCE  0
 //---------------------------MESAUREMENT PERIOD-------------------------------//
 #define     P_30S       6942
 #define     P_15S       36239
 #define     P_10S       46004
 #define     P_5S        55770
 
-short unsigned int period, count;
+short unsigned int count;
 short unsigned float h_rate;
+short int delay;
+char MEASURE;
 
 #pragma code high_vector    =   0x08
 void interrupt_high(void)
@@ -27,130 +36,103 @@ void interrupt_high(void)
 #pragma interrupt h_isr
 void h_isr(void)
 {
-    if (TMR1IF)
-    {
-        if(count != 0)
+    if (TMR1IF){
+        if (count > 0)
         {
-            init_timer();
+            TMR1IF  =   0;
             count--;
+            config_timer();
         }
         else
         {
-            stop_measure(&h_rate);
+            get_rate();
         }
     }
+    
 }
-
-#if 1
-void main(void)
+//only function that needs to be called
+//poll MEASURE to determine when read is available
+void init_measure(void)
 {
-    init_measure();
-    if (h_rate < 50 || h_rate > 90)
-    {
-        //alarm();
-    }
+    MEASURE =   1;
+    count = 64;
+    config_timer();
+    config_counter();
+}
+#if TEST
+void get_count(void)
+{
+    h_rate  =   ReadTimer0();
     return;
 }
 #endif
-void init_counter(void)
+void get_rate(void)
 {
-    GIE         =       0;
-    PEIE        =       0;
-    TMR0IF      =       0;
-    TMR0ON      =       0;
-    tmr0_rst();
-    
-    T0CS        =       1;                                                      //transition on T0CKI pin
-    T08BIT      =       1;                                                      //8-bit counter
-    T0SE        =       0;                                                      //increment on LOW-HIGH transition
-    PSA         =       0;                                                      //PRESCALER not assigned 
-    
+    TMR1_RST;
+    switch(delay)
+    {           
+        case 5:
+            h_rate      =   TMR0L/5;
+            MEASURE     =   0;
+            break;
+        case 10:
+            h_rate      =   TMR0L/10;
+            MEASURE     =   0;
+            break;
+        case 15:   
+            h_rate      =   TMR0L/15;
+            MEASURE     =   0;
+            break;
+        case 30:
+            h_rate      =   TMR0L/30;
+            MEASURE     =   0;
+            break;
+        default:    
+            h_rate      =    NULL;   
+            MEASURE     =   0;
+    }
+    TMR0_RST;
+}
+
+
+
+void config_counter(void)
+{
+    TMR0_RST;
+    OpenTimer0(TIMER_INT_OFF 
+            &   T0_8BIT
+            &   T0_SOURCE_EXT
+            &   T0_EDGE_RISE
+            &   T0_PS_1_1);
     return;
 }
 
-void init_timer(void)
+void config_timer(void)
 {
-    GIE         =       0;
-    TMR1IE      =       0;
-    TMR1ON      =       0;
-    RD16        =       1;                                                      //16-BIT operations    
-    T1RUN       =       0;                                                      
-    T1CKPS1     =       1;
-    T1CKPS0     =       1;
-    T1OSCEN     =       0;
-    TMR1CS      =       0;                                                      //internal clock source
-    
-    switch(period)
+    TMR1_RST;
+    switch(delay)
     {           
         case 5:
-            TMR1H   =   P_5S;
+            WriteTimer1(P_5S);
             break;
         case 10:
-            TMR1H   =   P_10S;
+            WriteTimer1(P_10S);
             break;
         case 15:   
-            TMR1H   =   P_15S;
+            WriteTimer1(P_15S);
             break;
         case 30:
-            TMR1H   =   P_30S;
+            WriteTimer1(P_30S);
             break;
         default:    
             return NULL;                             
     }
-    TMR1IF      =       0;
-    IPEN        =       1;                                                      //interrupt PRIORITY enables    
-    TMR1IP      =       1;                                                      //priority HIGH on overflow
-    PEIE        =       1;
-    TMR1IE      =       1;
-    GIE         =       1;
-    TMR1ON      =       1;
-    return;
+    OpenTimer1(TIMER_INT_ON
+            &   T1_16BIT_RW
+            &   T1_SOURCE_INT
+            &   T1_PS_1_8
+            &   T1_OSC1EN_OFF
+            &   T1_SYNC_EXT_OFF);
+    
 }
-
-void init_measure(void)
-{
-    count   =   64;    
-    init_counter();
-    init_timer();
-    return;
-}
-
-void stop_measure(short unsigned float *h_rate)
-{
-    TMR1IE      =       0;    
-    TMR1IF      =       0;
-    tmr1_rst();
-    switch(period)
-    {           
-        case 5:
-            *h_rate   =   TMR0L/5;
-            break;
-        case 10:
-            *h_rate   =   TMR0L/10;
-            break;
-        case 15:   
-            *h_rate   =   TMR0L/15;
-            break;
-        case 30:
-            *h_rate   =   TMR0L/30;
-            break;
-        default:    
-            *h_rate   =    NULL;                             
-    }
-    tmr0_rst();
-    return;
-}
-
-void tmr0_rst(void)
-{
-    TMR0H   =   0;
-    TMR0L   =   0;
-}
-
-void tmr1_rst(void)
-{
-    TMR1H   =   0;
-    TMR1L   =   0;
-}
-
 
