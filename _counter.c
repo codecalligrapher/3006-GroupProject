c03f45859dacc8f2d98e817b0ec95ccfb82ab9b4
aadi350 <a.aadi380@gmail.com>
2018-11-13 01:23:32 -0400

    Add files via upload

--- _counter.c
+++ _counter.c
@@ -5,49 +5,26 @@
 #include <xlcd.h>
 #include <portb.h>
 #include <timers.h>
+#include <spi.h>
 #include "_functions.h"
 #include "def_f842.h"
 
-
-
 #pragma config OSC = XT                                                         // High-Speed oscillator
 #pragma config WDT = OFF                                                        // Watchdog timer OFF
-#pragma config LVP = OFF      
+#pragma config LVP = OFF
 
 #define _XTAL_FREQ   4000000
 
-#define     NDEBUG_ISR      0
-#define     NDEBUG_LED      0
-#define     NDEBUG_KPAD     0
-#define     NDEBUG_TEMP     0
-#define     NDEBUG_COUNTER  0
-#define     NDEBUG          1
-#define     TEMP            0
-#define     HRATE           1
 
-#define     CLEAR_LCD       WriteCmdXLCD(0x01)
-
-static int  beat_count, i, pNN50_cnt, dbg;
-static int isr_count;
-unsigned int beats_time[60];
+static int  i;
 extern short unsigned int kpad_data;
-extern int interval;
 extern char Temperature[];
-float bpm, var;
 extern float finaltemp;
-char LCD_temp[16];
-char LCD_test[32];
-char LCD_bpm[32];
-char LCD_var[32];
-
-
-int beats_time_LCD;
-
-
-void LED_debug(void);
-void temp_debug(void);
-void kpad_debug(void);
-
+static char LCD_temp[16];
+static char LCD_bpm[32];
+static char LCD_rate[32];
+static char LCD_var[32];
+static char LCD_test[32];
 
 #pragma code high_vector=0x08
 void high_vector(void)
@@ -60,108 +37,120 @@ void high_vector(void)
 void h_isr(void)
 {
     GIE =   0;
+
+    if (INT2IF)
+    {
 #if NDEBUG
-    LED_TEST_THREE  =   1;
-#endif  
-#if NDEBUG_ISR
-        isr_count++;
+    LED_TEST_THREE  =   ~LED_TEST_THREE;
 #endif
-      
+        INT2IF  =   0;
+        time2_set(time_get());
+        time_set(ReadTimer0());
+        pnn50_calc(time_get(), time2_get());
+        countv_incr();
+#if NDEBUG_PNN50
+        LCD_NOT_READY;
+        CLEAR_LCD;
+        SetDDRamAddr(LINE_THREE);
+        sprintf(LCD_test, "PREV: %u", time2_get());
+        LCD_NOT_READY;
+        putsXLCD(LCD_test);
+        LCD_NOT_READY;
+        SetDDRamAddr(LINE_TWO);
+        sprintf(LCD_test, "CURR: %u", time_get());
+        LCD_NOT_READY;
+        putsXLCD(LCD_test);
+        LCD_NOT_READY;
+        SetDDRamAddr(LINE_THREE);
+        sprintf(LCD_test, "VAR CNT: %i", pnn50_get());
+        LCD_NOT_READY;
+        putsXLCD(LCD_test);     
+        
+#endif
+#if NDEBUG_COUNTER
+        LCD_NOT_READY;
+        SetDDRamAddr(LINE_ONE);
+        sprintf(LCD_test,"COUNT: %u, TIME: %u", countv_get(), ReadTimer0());
+        LCD_NOT_READY;
+        putsXLCD(LCD_test);
+#endif
+    }
+
     if(TMR0IF)
     {
 #if NDEBUG
         LED_TEST_ONE    =   1;
 #endif
         TMR0IF  =   0;
-        bpm =   ReadTimer1();
         
-        sprintf(LCD_bpm, "BPM: %d", ReadTimer1());
-        sprintf(LCD_var, "pNN50: %d", var);
+        sprintf(LCD_rate, "BPM: %u", countv_get_rate());
+        sprintf(LCD_var, "pNN50: %2.2f",3.142);// pnn50_var());
         LCD_NOT_READY;
         CLEAR_LCD;
         LCD_NOT_READY;
         SetDDRamAddr(LINE_ONE);
         LCD_NOT_READY;
-        putsXLCD(LCD_bpm);
+        putsXLCD(LCD_rate);
         LCD_NOT_READY;
         SetDDRamAddr(LINE_TWO);
         LCD_NOT_READY;
         putsXLCD(LCD_var);
-#if NDEBUG_ISR
-        sprintf(LCD_test,"ISR: %i",isr_count);
-        LCD_NOT_READY;
-        CLEAR_LCD;
-        LCD_NOT_READY;
-        SetDDRamAddr(LINE_THREE);
-        LCD_NOT_READY;
-        putsXLCD(LCD_test);
-#endif
-        WriteTimer1(0);
+        countv_reset();
+        time_reset();
+        time2_reset();
         WriteTimer0(0);
     }
-#if NDEBUG
-    LED_TEST_THREE  =  0; 
+#if 0
+    LED_TEST_THREE  =  0;
 #endif
     GIE =   1;
 }
 
 void counter_measure_config(void)
 {
-    bpm   = 0;
-    TRISBbits.TRISB0    =   1;      //make input
-    PORTBbits.INT0      =   1;
+    TRISBbits.TRISB2    =   1;      //make input
     TRISCbits.TRISC0    =   1;
     TRISCbits.RC0       =   1;
-    
+
+    countv_reset();
+    time_reset();
+    pnn50_reset();
     
     T0CON   =   0x87;
-    WriteTimer0(30380);
+    WriteTimer0(6942);
     TMR0IE  =   1;
     RBPU    =   0;      //individual PULLUPS enabled
     IPEN    =   1;      //enable priority
-    INT0IF  =   0;      //clear flag    
-    WriteTimer1(0);
-    OpenTimer1(TIMER_INT_OFF
-                & T1_8BIT_RW
-                & T1_SOURCE_EXT
-                & T1_PS_1_1
-                & T1_OSC1EN_OFF
-                & T1_SYNC_EXT_OFF
-                & T3_SOURCE_CCP
-            );
-    //INTEDG0 =   1;      //select rising edge
-    //INT0IE  =   1;      //enable external interrupt    
+    INT2IF  =   0;      //clear flag
+    INTEDG2 =   1;      //select rising edge
+    INT2IE  =   1;      //enable external interrupt
+    INT2IP  =   1;
     GIE     =   1;      //enable HIGH interrupts
     PEIE    =   1;      //enable LOW interrupts
     return;
 }
 
-
-
-
 double counter_bpm(void)
-{   
+{
     int count_temp  =   ReadTimer1();
     return (count_temp*6);
 }
 
 
 void main(void)
-{   
+{
     int LCD_put     =   0;
-    isr_count       =   1;    
-  
-    
+
     //choose option for interval storage
 #if NDEBUG
     LED_debug();
     temp_debug();
     kpad_debug();
-#endif  
+    glucose_debug();
+#endif
 
-    
-    TRISBbits.TRISB0 = 1;  
-    
+    TRISBbits.TRISB0 = 1;
+
 #if !NDEBUG
     while(1)
     {
@@ -169,25 +158,18 @@ void main(void)
 #if NDEBUG
         LED_TEST_TWO    =   1;
 #endif
-        
+
 #if HRATE
+        putrsXLCD("MEASURING");
         counter_measure_config();
-        while(1){
-#if NDEBUG_COUNTER
-            sprintf(LCD_test,"TEST: %i",ReadTimer1());
-            CLEAR_LCD;
-            LCD_NOT_READY;
-            putsXLCD(LCD_test);
+        while(1);
 #endif
-            
-        };
-#endif
-       
+
 #if NDEBUG
         LED_TEST_TWO    =   0;
 #endif
-        
-        
+
+
 #if TEMP
                 temp();
                 LCD_NOT_READY;
@@ -199,85 +181,4 @@ void main(void)
     }
 #endif
     return;
-}
-
-void LED_debug(void)
-{
-
-    TRISEbits.TRISE0    =   0;
-    TRISEbits.TRISE1    =   0;
-    TRISEbits.TRISE2    =   0;
-    LED_TEST_ONE        =   1;
-    LED_TEST_TWO        =   1;
-    LED_TEST_THREE      =   1;
-    Delay1KTCYx(256);
-    LED_TEST_ONE        =   0;
-    Delay1KTCYx(256);
-    LED_TEST_TWO        =   0;
-    Delay1KTCYx(256);
-    LED_TEST_THREE      =   0;
-    TRISD   =   0b00000000;
-    OpenXLCD(FOUR_BIT & LINES_5X7);   
-    LCD_NOT_READY;
-    WriteCmdXLCD(0x01);
-    LCD_NOT_READY;
-    
-#if NDEBUG_LED
-    while(1)
-    {
-        LED_TEST_ONE    =  0;
-        LED_TEST_TWO    =  0;
-        LED_TEST_THREE  =  0;
-        Delay1KTCYx(256);
-        Delay1KTCYx(256);
-        Delay1KTCYx(256);
-        Delay1KTCYx(256);
-        LED_TEST_ONE    =  1;
-        LED_TEST_TWO    =  1;
-        LED_TEST_THREE  =  1;
-        Delay1KTCYx(256);
-        Delay1KTCYx(256);
-        Delay1KTCYx(256);
-        Delay1KTCYx(256);
-    }
-#endif
-
-    return;
-}
-
-void temp_debug(void)
-{
-#if NDEBUG_TEMP
-    temp();
-    LCD_NOT_READY;
-    CLEAR_LCD;
-    LCD_NOT_READY;
-    SetDDRamAddr(0x40);
-    sprintf(LCD_test, "Temperature: %f", finaltemp);
-    LCD_NOT_READY;
-    putsXLCD(LCD_test);
-    LED_TEST_ONE    =   1;
-    while(1);
-#endif
-}
-
-void kpad_debug(void)
-{
-#if NDEBUG_KPAD
-    kpad_config();
-    while(1)
-    {
-        LED_TEST_ONE    =   1;
-        while(!PORTBbits.INT1);
-        
-        kpad_data = kpad_rslv();
-        sprintf(LCD_test,"Keypress: %c", kpad_data);
-        LCD_NOT_READY;
-        SetDDRamAddr(LINE_ONE);
-        CLEAR_LCD;
-        LCD_NOT_READY;
-        putsXLCD(LCD_test);
-        LCD_NOT_READY;
-    }
-#endif
 }
