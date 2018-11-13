9d26fbf46e0f62a018c4d75c34d11dc1cade9491
aadi350 <a.aadi380@gmail.com>
2018-11-13 01:26:13 -0400

    Add files via upload

--- _functions.h
+++ _functions.h
@@ -9,45 +9,70 @@
 
 
 
-
-short unsigned int kpad_read(void);
-unsigned char kpad_rslv(void);
-/*_adc.c*/
+/*_kpad.c*/
+char kpad_rslv(void);
 void kpad_config(void);
-int glucose(void);
-void h_isr(void);
-void measure(void);
-/*heartrate.c*/
-void config_timer(void);                                                        //configures timer1 for required interval of measurement
-void config_counter(void);                                                      //configures timer0 to begin counting
-void get_rate(void);
-void get_count(void);
-void init_measure(void);
+
+/*_adc.c*/
+int adc_measure(void);
+
+/*_lcd.c*/
+void LCD_config(void);
 
 /*_counter.c*/
 void h_isr(void);
-void main(void);
-int calc_rate(int period);
-void measure_beats(void);
+void    counter_measure_config(void);
+double  counter_bpm(void);
 
+/*_interface.c*/
+void interface_action(void);
+void interface_display_abort(void);
+void interface_interval(void);
+void interface_set_temp(float);
+void interface_set_rate(int);
+void interface_set_pnn50(float);
+
+/*_countv.c*/
+void countv_incr(void);
+int countv_get(void);
+int countv_get_rate(void);
+void countv_reset(void);
+void time_set(int);
+int time_get(void);
+void time_reset(void);
+void time2_reset(void);
+int time2_get(void);
+void time2_set(int);
+
+/*_dbg.c*/
+void LED_debug(void);
+void temp_debug(void);
+void kpad_debug(void);
+void glucose_debug(void);
+
+/*_temp.c*/
+void temp(void);
+float temp_get(void);
+
+/*_pNN50.c*/
+void pnn50_calc(int, int);
+void pnn50_reset(void);
+float pnn50_var(void);
+int pnn50_get(void);
 
 #define LCD_NOT_READY   while(BusyXLCD());
 
 //-----------------------------PORT DEFINITIONS-------------------------------//
 #define kpad_d0     PORTDbits.RD0
 #define kpad_d1     PORTDbits.RD1
-#define kpad_d2     PORTAbits.RA3 
-//#define kpad_d2     PORTDbits.RD2
+#define kpad_d2     PORTCbits.RC2
+//#define kpad_d2   //PORTDbits.RD2
 #define kpad_d3     PORTDbits.RD3
 #define kpad_rdy    PORTBbits.RB2 
 
 #define kpad_t0     TRISDbits.TRISD0
 #define kpad_t1     TRISDbits.TRISD1
-#define kpad_t2     TRISAbits.TRISA3
+#define kpad_t2     TRISCbits.TRISC2
 //#define kpad_t2     TRISDbits.TRISD2
 #define kpad_t3     TRISDbits.TRISD3
 #define kpad_rdyt   TRISBbits.TRISB2
-
-
-//------------------------------BIT DEFINITIONS-------------------------------//
-#define data_rdy    INTCON3bits.INT2IF
---
+++ def_f842.h
@@ -0,0 +1,139 @@
+/* 
+ * File:   def_f842.h
+ * Author: aaadi
+ *
+ * Created on October 29, 2018, 5:30 PM
+ */
+#include <p18f452.h>
+
+#define     NDEBUG_GLUCOSE  0
+#define     NDEBUG_ISR      0
+#define     NDEBUG_KPAD     0
+#define     NDEBUG_LED      0
+#define     NDEBUG_TEMP     0
+#define     NDEBUG_COUNTER  0
+#define     NDEBUG_PNN50    1
+#define     NDEBUG          1
+#define     TEMP            0
+#define     HRATE           1
+
+#define     GIE         INTCONbits.GIE
+#define     GIEH        INTCONbits.GIEH
+#define     PEIE        INTCONbits.PEIE
+#define     TMR0IE      INTCONbits.TMR0IE
+#define     INT0IE      INTCONbits.INT0IE
+#define     RBIE        INTCONbits.RBIE
+#define     TMR0IF      INTCONbits.TMR0IF
+#define     INT0IF      INTCONbits.INT0F
+#define     RBIF        INTCONbits.RBIF
+
+#define     RBPU        INTCON2bits.NOT_RBPU
+#define     INTEDG0     INTCON2bits.INTEDG0
+#define     INTEDG1     INTCON2bits.INTEDG1
+#define     INTEDG2     INTCON2bits.INTEDG2
+#define     TMR0IP      INTCON2bits.TMR0IP
+#define     RBIP        INTCON2bits.RBIP
+
+#define     INT2IP      INTCON3bits.INT2IP
+#define     INT1IP      INTCON3bits.INT1IP
+#define     INT1IE      INTCON3bits.INT1IE
+#define     INT2IF      INTCON3bits.INT2IF
+#define     INT2IE      INTCON3bits.INT2IE
+#define     INT1IF      INTCON3bits.INT1F
+
+#define     PSPIF       PIR1bits.PSPIF
+#define     ADIF        PIR1bits.ADIF
+#define     RCIF        PIR1bits.RCIF
+#define     TXIF        PIR1bits.TXIF
+#define     SSPIF       PIR1bits.SSPIF
+#define     CCP1IF      PIR1bits.CCP1IF
+#define     TMR2IF      PIR1bits.TMR2IF
+#define     TMR1IF      PIR1bits.TMR1IF
+
+#define     EEIF        PIR2bits.EEIF
+#define     BCLIF       PIR2bits.BCLIF
+#define     LVDIF       PIR2bits.LVDIF
+#define     TMR3IF      PIR2bits.TMR3IF
+#define     CCP2IF      PIR2bits.CCP2IF
+
+
+#define     PSPIE       PIR1bits.PSPIE
+#define     ADIE        PIR1bits.ADIE
+#define     RCIE        PIR1bits.RCIE
+#define     TXIE        PIR1bits.TXIE
+#define     SSPIE       PIR1bits.SSPIE
+#define     CCP1IE      PIR1bits.CCP1IE
+#define     TMR2IE      PIR1bits.TMR2IE
+#define     TMR1IE      PIR1bits.TMR1IE
+
+#define     EEIE        PIR2bits.EEIE
+#define     BCLIE       PIR2bits.BCLIE
+#define     LVDIE       PIR2bits.LVDIE
+#define     TMR3IE      PIR2bits.TMR3IE
+#define     CCP2IE      PIR2bits.CCP2IE
+
+#define     PSPIP       IPR1bits.PSPIP
+#define     ADIP        IPR1bits.ADIP
+#define     RCIP        IPR1bits.RCIP
+#define     TXIP        IPR1bits.TXIP
+#define     SSPIP       IPR1bits.SSPIP
+#define     CCPIP       IPR1bits.TMR1IP
+#define     TMR2IP      IPR1bits.TMR2IP
+#define     TMR1IP      IPR1bits.TMR1IP
+
+#define     EEIP        IPR2bits.EEIP
+#define     BCLIP       IPR2bits.BCLIP
+#define     LVDIP       IPR2bits.LVDIP
+#define     TMR3IP      IPR2bits.TMR3IP
+#define     CCP2IP      IPR2bits.CCP2IP
+
+#define     IPEN        RCONbits.IPEN
+#define     NOT_RI      RCONbits.NOT_RI
+#define     NOT_TO      RCONbits.NOT_TO
+#define     NOT_PD      RCONbits.NOT_PD
+#define     NOT_POR     RCONbits.NOT_POR
+#define     NOT_BOR     RCONbits.NOT_BOR
+
+#define     TMR0ON      T0CONbits.TMR0ON
+#define     T08BIT      T0CONbits.T08BIT
+#define     T0CS        T0CONbits.T0CS
+#define     T0SE        T0CONbits.T0SE
+#define     PSA         T0CONbits.PSA
+#define     T0PS2       T0CONbits.T0PS2
+#define     T0PS1       T0CONbits.T0PS1
+#define     T0PS0       T0CONbits.T0PS0
+
+#define     RD16        T1CONbits.RD16
+#define     T1CKPS1     T1CONbits.T1CKPS1
+#define     T1CKPS0     T1CONbits.T1CKPS0
+#define     T1OSCEN     T1CONbits.T1OSCEN
+#define     T1SYNC      T1CONbits.T1INSYNC
+#define     TMR1CS      T1CONbits.TMR1CS
+#define     TMR1ON      T1CONbits.TMR1ON
+
+#define     EEPGD       EECON1bits.EEPGD
+#define     CFGS        EECON1bits.CFGS
+#define     FREE        EECON1bits.FREE
+#define     WRERR       EECON1bits.WRERR
+#define     WREN        EECON1bits.WREN
+#define     WR          EECON1bits.WR
+#define     RD          EECON1bits.RD
+
+#define LINE_ONE    0x00
+#define LINE_TWO    0x40
+#define LINE_THREE  0x10
+#define LINE_FOUR   0x50
+
+#define ONE_SECOND_PS_1_256 3906.25 
+#define TWO_SECOND_PS_1_256 7813 
+#define FIFTY_MSECOND_PS_1_256 195
+
+#define     LED_TEST_ONE    LATEbits.LATE0
+#define     LED_TEST_TWO    LATEbits.LATE1
+#define     LED_TEST_THREE  LATEbits.LATE2
+#define     LED_TEST_ONE_STAT   PORTEbits.RE0
+#define     LED_TEST_TWO_STAT   PORTEbits.RE1
+#define     LED_TEST_THREE_STAT PORTEbits.RE2
+
+
+#define     CLEAR_LCD       WriteCmdXLCD(0x01)
