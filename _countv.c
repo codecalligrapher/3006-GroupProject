de48e9096c3ab849289c45d3cc19603b20439678
aadi350 <a.aadi380@gmail.com>
2018-11-13 01:23:58 -0400

    Shifted around functions, reduction in number of Global Variables

---
+++ _countv.c
@@ -0,0 +1,63 @@
+#include <p18f452.h>
+#include <timers.h>
+#include <math.h>
+#include "_functions.h"
+#include "def_f842.h"
+
+static unsigned int countv_beats    = 0;
+static unsigned int countv_time     = 0;
+static unsigned int countv_time2    = 0;
+
+void countv_incr(void)
+{
+    countv_beats++;
+    return;
+}
+
+int countv_get(void)
+{
+    return countv_beats;
+}
+
+void countv_reset(void)
+{
+    countv_beats = 0;
+    return;
+}
+
+int countv_get_rate(void)
+{
+    return (4*countv_beats);
+}
+
+void time_set(int timer_value)
+{
+    countv_time = timer_value;
+    return;
+}
+
+int time_get(void)
+{
+    return countv_time;
+}
+
+void time_reset(void)
+{
+    countv_time =   0;
+    return;
+}
+
+void time2_reset(void)
+{
+    countv_time2    =   0;
+    return;
+}
+int time2_get(void)
+{
+    return countv_time2;
+}
+void time2_set(int timer_value)
+{
+    countv_time2    =   timer_value;
+    return;
+}
